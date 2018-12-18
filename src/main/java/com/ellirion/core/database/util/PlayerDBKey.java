1cd697a219b153948685f33a9f2f042010f397f6
Nick Kronenberg <nikro412956@gmail.com>
2018-12-18 14:55:03 +0100

    commented code removed

--- src/main/java/com/ellirion/core/database/util/PlayerDBKey.java
+++ src/main/java/com/ellirion/core/database/util/PlayerDBKey.java
@@ -27,9 +27,4 @@ public class PlayerDBKey {
         this.gameID = gameID;
         this.playerID = playerID;
     }
-    //
-    //    @Override
-    //    public String toString() {
-    //        return gameID.toString() + " , " + playerID.toString();
-    //    }
 }
